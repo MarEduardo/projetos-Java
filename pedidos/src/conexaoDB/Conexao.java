73b1bfccfcd8fbd5a5a8528a13983ea7cab51a2f
MarEduardo <136724561+MarEduardo@users.noreply.github.com>
2023-08-16 11:34:31 -0300

    Add files via upload

---
+++ pedidos/src/conexaoDB/Conexao.java
@@ -0,0 +1,64 @@
+package conexaoDB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Conexao {
+	public static Connection criarConexao() {
+		String driver = "com.mysql.cj.jdbc.Driver";
+		String servidor = "jdbc:mysql://localhost:3306/banco";
+		String usuario = "root";
+		String senha = "polimigmysql";
+		try {
+			Class.forName(driver);
+			return DriverManager.getConnection(servidor,usuario,senha);
+		}
+		catch (Exception e){
+			System.out.println("Erro ao conectar: " + e.getMessage());
+			return null;
+			
+			
+		}
+	}
+	
+	public static void fecharConexao(Connection conn) {
+		if (conn != null) {
+			try {
+				conn.close();
+			}
+			catch (Exception e) {
+				System.out.println ("Erro ao fechar a conexão: "+ e.getMessage());
+			}
+		}
+		
+		
+	}
+	
+	public static void fecharConexao(Connection conn, PreparedStatement stat) {
+		Conexao.fecharConexao(conn,stat);
+		if (stat != null) {
+			try {
+				stat.close();
+			}
+			catch(SQLException e) {
+				System.out.println("Erro ao fechar conexão: " + e.getMessage());
+			}
+		}
+	}
+	
+	public static void fecharConexao(Connection conn, PreparedStatement stat, ResultSet res) {
+		Conexao.fecharConexao(conn,stat);
+		if (res != null) {
+			try {
+				res.close();
+			}
+			catch(Exception e) {
+				System.out.println("Erro ao fechar conexão: " + e.getMessage());
+			}
+		}
+	}
+	
+}
